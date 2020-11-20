90ba48c70336c74d36d32c069d1b345a015b7b1d
damian <dam.strojny@gmail.com>
2020-11-20 06:15:34 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,23 +1,18 @@
-def mi(a, m): 
-    mod0 = m 
-    y = 0
-    x = 1
-    if (m == 1): 
-        return 0
-    while (a > 1):
-        q = a // m 
-        t = m 
-        m = a % m 
-        a = t 
-        t = y 
-        y = x - q * y 
-        x = t 
-    if (x < 0): 
-        return x + mod0 
-    return x 
-  
+b = eval(input("wprowadz b: "))
+n = eval(input("wprowadz n: "))
 
-if __name__ == "__main__":
-    print(mi(25, 4))
-    print(mi(15, 122))
-    print(mi(11, 37))
+
+def euc(b, n):
+
+    if n == 0:
+        return (1, 0, b)
+    q, r = b // n, b % n
+    x, y, d = euc(n, r)
+    s, t = y, x - q * y
+    return s, t, d  
+
+def inverse(b, N):
+
+ return euc(b, N)[0]
+
+print(inverse(b,n))
