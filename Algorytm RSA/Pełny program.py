1896086038297ed8bf6a36c562a0282ec7ea1c72
damian <dam.strojny@gmail.com>
2020-12-10 18:15:43 +0100

    Update Pełny program.py

--- Algorytm RSA/Pełny program.py
+++ Algorytm RSA/Pełny program.py
@@ -1,38 +1,110 @@
-p = 1298074214633706907132624082305003
-q = 1298074214633706907132624082304809
-n = p*q
-e = 1172187857271832809276226663461802240700459848485291886282435086085
-m = 123456789
+import random
 
-def euc(p, n):
+#zad 6
+def primef(n,d):
+    if n == 1: return False
+    if n == 2: return True
+    for k in range(100):
+        a = random.randint(2, n - 1)
+        if nwd(n, a) != 1:
+            return False
+        if pow(a, n - 1, n) != 1:
+            return False
+    return True
+
+def nwd(x, y):
+    while y > 0:
+        x, y = y, x % y
+    return x
+
+
+
+def generate_keys(key):  # Generate P and Q and other variables to act as keys
+    p = generate_prime(key)
+    q = generate_prime(key)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    while True:
+        e = random.randrange(2 ** (key - 1), 2 ** key - 1)
+        if is_CoPrime(e, phi):
+            break
+
+    d = inverse(e, phi)
+    return p, q, n, phi, e, d
+
+def isPrime(n):
+    if (n==1):
+        return False
+    elif (n==2):
+        return True;
+    else:
+        for x in range(2,n):
+            if(n % x==0):
+                return False
+        return True   
+
+def is_CoPrime(d, Phi):  
+    return nwd(d, Phi) == 1
+
+def generate_prime(keysize):  
+    while True:
+        num = random.randrange(2 ** (keysize - 1), 2 ** keysize - 1)
+        if isPrime(num):
+            return num
+#zad 2
+def euc(b, n):
 
     if n == 0:
-        return (1, 0, p)
-    q, r = p // n, p % n
+        return (1, 0, b)
+    q, r = b // n, b % n
     x, y, d = euc(n, r)
     s, t = y, x - q * y
     return s, t, d  
 
-def inverse(p, n):
+def inverse(b, n):
 
- return euc(p, n)[0]
-
-phi = (p - 1) * (q - 1)
-d = inverse(e, phi)
+ return euc(b, n)[0]
 
 
+def encrypt(e, N, msg):  
+    cipher = ""
+    for c in msg:
+        m = ord(c)
+        cipher += str(pow(m, e, N)) + " "
+
+    return cipher
 
 
+def decrypt(d, N, cipher): 
+    msg = ""
+    parts = cipher.split()
+    for part in parts:
+        if part:
+            c = int(part)
+            msg += chr(pow(c, d, N))
+
+    return msg
 
 
-if(p != "" and q  != "" and e != ""):
+if __name__ == '__main__':
+    KeySize = 16
+    p, q, N, PN, e, d = generate_keys(KeySize)
+    
+    print("Klucz publiczny:\nn = ",N,"\ne = ",e,"\nKlucz tajny:\nn = ",N,"\nd = ",d,"\n")
 
- print("1) Generowanie klucza tajnego i publicznego\n2) Szyfrowanie\n3) Deszyfrowanie\n")
- g = input("Wprowadź liczbę: ")
- if(g=="1"): 
-    print("Klucz publiczny:\nn = ",n,"\ne = ",e,"\nklucz tajny:\nn = ",n,"\nd = ",d)
- elif(g == "2"):
-    print("in progress")
- elif(g == "3"):
-    print("in progress")
-   
+    
+              
+print ("                     #####SZYFROWANIE#####\n")
+print(f"e: {e}")
+print(f"N: {N}")
+msg = input("Wprowadź wiadomość do zaszyfrowania: ")
+enc = encrypt(e, N, msg)
+print(f"\nZaszyfrowana wiadomość: [ {enc}]\n")
+print ("                    #####DESZYFROWANIE#####\n")
+print(f"d: {d}")
+print(f"N: {N}\n")
+msg = input("Wprowadź wiadomość do odszyfrowania: ")
+dec = decrypt(d, N, msg)
+print(f"Odszyfrowana wiadomość: [{dec}]")
+           
