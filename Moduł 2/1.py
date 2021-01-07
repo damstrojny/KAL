324ce3f04584a387b62589f9edb35050f2a62e7b
damian <dam.strojny@gmail.com>
2021-01-08 00:05:25 +0100

    Update 1.py

--- Moduł 2/1.py
+++ Moduł 2/1.py
@@ -1,13 +1,5 @@
 import random
 
-def binpow(x, y, z):
-    number = 1
-    while y:
-        if y & 1:
-            number = number * x % z
-        y >>= 1
-        x = x * x % z
-    return number
 
 def generate(p):
     a = random.randint(2, p)
@@ -15,8 +7,7 @@ def generate(p):
     return a, b
 
 
-p = 68719476731
-
+p = eval(input("wpisz p: "))
 print("p =", p,"\n")
 a, b = generate(p)
 print("A =", a,"\nB =", b)
